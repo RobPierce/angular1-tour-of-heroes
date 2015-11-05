023a29cf06086ae10b3afdacf570fed4d5672907
Frank van Wijk <fvanwijk@xebia.com>
2015-11-05 11:39:28 +0100

    Add upgrade code (commented) to show what to do when you upgrade using ngUpgrade

--- src/angular1/bootstrap.ts
+++ src/angular1/bootstrap.ts
@@ -7,7 +7,10 @@ import {HeroDetailController} from './detail/hero-detail.component';
 import {HeroService} from './hero.service';
 import {RouterConfig} from './router.config';
 
-angular.module('tourOfHeroes', ['ngRoute'])
+var app = angular.module('tourOfHeroes', ['ngRoute']);
+
+// Angular 1 bootstrap
+app
   .config(RouterConfig)
   .controller({
     AppController: AppController,
@@ -16,3 +19,26 @@ angular.module('tourOfHeroes', ['ngRoute'])
     HeroDetailController: HeroDetailController
   })
   .service('HeroService', HeroService);
+
+
+// Angular 2 upgrade bootstrap
+/*
+ * import {UpgradeAdapter} from 'angular2/upgrade';
+ * var adapter = new UpgradeAdapter();
+ *
+ * adapter.bootstrap(document.body, ['tourOfHeroes']);
+ *
+ * // Make ng2 component available in ng1
+ * app.directive('heroes', adapter.downgradeNg2Component(HeroesComponent);
+ *
+ * // Make ng1 HeroService injectable into ng2 HeroesComponent
+ * adapter.upgradeNg1Provider(HeroService, { asToken: HeroService });
+ *
+ * // Make ng2 HeroService injectable into ng2 HeroesComponent
+ * adapter.addProvider(HeroService)
+ *
+ * // Make ng2 HeroService injectable into ng2 HeroesComponent
+ * app.factory('HeroService', adapter.downgradeNg2Provider(HeroService));
+ */
+
+
