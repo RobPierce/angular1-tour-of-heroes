19f3c9ecd00fe4265ddf6dade1eea0d233909983
Frank van Wijk <fvanwijk@xebia.com>
2015-11-09 00:17:10 +0100

    Add adapter files

---
+++ src/adapter.js
@@ -0,0 +1,3 @@
+var upgrade_1 = require('./angular2/upgrade');
+exports.adapter = new upgrade_1.UpgradeAdapter();
+//# sourceMappingURL=adapter.js.map
---
+++ src/angular2/upgrade.js
@@ -0,0 +1,3 @@
+var upgrade_adapter_1 = require('./src/upgrade_adapter');
+exports.UpgradeAdapter = upgrade_adapter_1.UpgradeAdapter;
+//# sourceMappingURL=upgrade.js.map
