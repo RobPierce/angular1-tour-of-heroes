ce6cf4d4e6de96166bc4430633c8400e4dc62270
Frank van Wijk <fvanwijk@xebia.com>
2015-11-09 00:14:31 +0100

    Cleanup typing folder structure

--- typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../src/typings/angularjs/angular-route.d.ts" />
-/// <reference path="../src/typings/angularjs/angular.d.ts" />
-/// <reference path="../src/typings/es6-promise/es6-promise.d.ts" />
-/// <reference path="../src/typings/jquery/jquery.d.ts" />
